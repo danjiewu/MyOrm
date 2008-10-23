5e6ab32a2ea21346131b865213debeed23be5e36
danjiewu_cp <SND\danjiewu_cp>
2008-10-23 08:30:08 +0000

    Changed log format.

--- Samples/Sample3/RemoteAPI/RemoteDispatcher.cs
+++ Samples/Sample3/RemoteAPI/RemoteDispatcher.cs
@@ -19,7 +19,7 @@ namespace Northwind.RemoteAPI
 
     public class HttpDispatcher : IRemoteDispatcher
     {
-        private static ILog logger = LogManager.GetLogger("Northwind");
+        private static ILog logger = LogManager.GetLogger("HttpDispatcher");
         #region IRemoteDispatcher Members
 
         public object RemoteInvoke(string serviceName, MethodInfo method, object[] args)
@@ -39,7 +39,7 @@ namespace Northwind.RemoteAPI
                 request.ContentLength = ms.Length;
 
                 string strArgs = String.Join(",", Array.ConvertAll<object, string>(args, delegate(object o) { return o == null ? "null" : Convert.ToString(o); }));
-                logger.DebugFormat("[HttpDispatcher] DAO:{{{0}}} Method:{{{1}}} invoked, Args:{{{2}}}. {3} bytes to send.", serviceName, method.Name, strArgs, ms.Length);
+                logger.DebugFormat("DAO:{{{0}}} Method:{{{1}}} invoked, Args:{{{2}}}. {3} bytes to send.", serviceName, method.Name, strArgs, ms.Length);
 
                 using (Stream inputStream = request.GetRequestStream())
                 {
@@ -51,17 +51,19 @@ namespace Northwind.RemoteAPI
             {
                 using (Stream outputStream = response.GetResponseStream())
                 {
+
+                    bool success = (bool)serializer.Deserialize(outputStream);
+                    object ret = serializer.Deserialize(outputStream);
                     try
                     {
-                        bool success = (bool)serializer.Deserialize(outputStream);
                         if (success)
-                            return serializer.Deserialize(outputStream);
+                            return ret;
                         else
-                            throw (Exception)serializer.Deserialize(outputStream);
+                            throw (Exception)ret;
                     }
                     finally
                     {
-                        logger.DebugFormat("[HttpDispatcher] DAO:{{{0}}} Method:{{{1}}} finished. {2} bytes recieved.", serviceName, method.Name, response.ContentLength);
+                        logger.DebugFormat("DAO:{{{0}}} Method:{{{1}}} finished. Returns:{{{2}}}. {3} bytes recieved.", serviceName, method.Name, ret, response.ContentLength);
                     }
 
                 }
@@ -73,7 +75,7 @@ namespace Northwind.RemoteAPI
 
     public class WebServiceDispatcher : IRemoteDispatcher
     {
-        private static ILog logger = LogManager.GetLogger("Northwind");
+        private static ILog logger = LogManager.GetLogger("WebServiceDispatcher");
         #region IRemoteDispatcher Members
 
         public object RemoteInvoke(string serviceName, MethodInfo method, object[] args)
@@ -86,10 +88,11 @@ namespace Northwind.RemoteAPI
                 serializer.Serialize(ms, method);
                 serializer.Serialize(ms, args);
                 string strArgs = String.Join(",", Array.ConvertAll<object, string>(args, delegate(object o) { return o == null ? "null" : Convert.ToString(o); }));
-                logger.DebugFormat("[WebServiceDispatcher] DAO:{{{0}}} Method:{{{1}}} invoked, Args:{{{2}}}. {3} bytes to send.", serviceName, method.Name, strArgs, ms.Length);
+                logger.DebugFormat("DAO:{{{0}}} Method:{{{1}}} invoked, Args:{{{2}}}. {3} bytes to send.", serviceName, method.Name, strArgs, ms.Length);
                 MemoryStream output = new MemoryStream(service.SerializedInvoke(ms.ToArray()));
-                logger.DebugFormat("[WebServiceDispatcher] DAO:{{{0}}} Method:{{{1}}} finished. {2} bytes recieved.", serviceName, method.Name, output.Length);
-                return serializer.Deserialize(output);
+                object ret = serializer.Deserialize(output);
+                logger.DebugFormat("DAO:{{{0}}} Method:{{{1}}} finished. Returns:{{{2}}}. {3} bytes recieved.", serviceName, method.Name, ret, ms.Length);
+                return ret;
             }
         }
 
