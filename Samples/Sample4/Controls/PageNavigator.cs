989d38bb0c9da33fc848ee89e2c9abead5547789
danjiewu_cp <SND\danjiewu_cp>
2008-10-24 07:43:21 +0000

    Add a paged object source sample.

---
+++ Samples/Sample4/Controls/PageNavigator.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace EE_Veloce
+{
+    public class PageNavigator : BindingNavigator
+    {
+        private ToolStripItem _currentPageItem;
+        private ToolStripItem _moveFirstPageItem;
+        private ToolStripItem _moveLastPageItem;
+        private ToolStripItem _moveNextPageItem;
+        private ToolStripItem _movePreviousPageItem;
+        private string _pageCountFormat;
+        private ToolStripItem _pageCountItem;
+        private PagedBindingSource _pageSource;
+
+        public override void AddStandardItems()
+        {
+            MoveFirstPageItem = new ToolStripButton();
+            MovePreviousPageItem = new ToolStripButton();
+            MoveNextPageItem = new ToolStripButton();
+            MoveLastPageItem = new ToolStripButton();
+            CurrentPageItem = new ToolStripTextBox();
+            PageCountItem = new ToolStripLabel();
+            ToolStripSeparator separator = new ToolStripSeparator();
+            ToolStripSeparator separator2 = new ToolStripSeparator();
+            char ch = string.IsNullOrEmpty(base.Name) || char.IsLower(base.Name[0]) ? 'p' : 'P';
+            MoveFirstPageItem.Name = ch + "ageNavigatorMoveFirstPageItem";
+            MovePreviousPageItem.Name = ch + "ageNavigatorMovePreviousPageItem";
+            MoveNextPageItem.Name = ch + "ageNavigatorMoveNextPageItem";
+            MoveLastPageItem.Name = ch + "ageNavigatorMoveLastPageItem";
+            CurrentPageItem.Name = ch + "ageNavigatorCurrentPageItem";
+            PageCountItem.Name = ch + "ageNavigatorPageCountItem";
+            separator.Name = ch + "ageNavigatorSeparator";
+            separator2.Name = ch + "ageNavigatorSeparator";
+            MoveFirstPageItem.Text = "PageNavigatorMoveFirstPageItemText";
+            MovePreviousPageItem.Text = "PageNavigatorMovePreviousPageItemText";
+            MoveNextPageItem.Text = "PageNavigatorMoveNextPageItemText";
+            MoveLastPageItem.Text = "PageNavigatorMoveLastPageItemText";
+            PageCountItem.ToolTipText = "PageNavigatorPageCountItemTip";
+            CurrentPageItem.ToolTipText = "PageNavigatorCurrentPageItemTip";
+            PageCountItem.AutoToolTip = false;
+            PageCountItem.Text = "/0";
+            CurrentPageItem.AutoToolTip = false;
+            CurrentPageItem.Text = "0";
+            CurrentPageItem.AccessibleName = "PageNavigatorPositionAccessibleName";
+            PageCountFormat = "/{0}";
+            Bitmap bitmap1 = new Bitmap(typeof(BindingNavigator), "BindingNavigator.MoveFirst.bmp");
+            Bitmap bitmap2 = new Bitmap(typeof(BindingNavigator), "BindingNavigator.MovePrevious.bmp");
+            Bitmap bitmap3 = new Bitmap(typeof(BindingNavigator), "BindingNavigator.MoveNext.bmp");
+            Bitmap bitmap4 = new Bitmap(typeof(BindingNavigator), "BindingNavigator.MoveLast.bmp");
+            bitmap1.MakeTransparent(Color.Magenta);
+            bitmap2.MakeTransparent(Color.Magenta);
+            bitmap3.MakeTransparent(Color.Magenta);
+            bitmap4.MakeTransparent(Color.Magenta);
+            MoveFirstPageItem.Image = bitmap1;
+            MovePreviousPageItem.Image = bitmap2;
+            MoveNextPageItem.Image = bitmap3;
+            MoveLastPageItem.Image = bitmap4;
+            MoveFirstPageItem.RightToLeftAutoMirrorImage = true;
+            MovePreviousPageItem.RightToLeftAutoMirrorImage = true;
+            MoveNextPageItem.RightToLeftAutoMirrorImage = true;
+            MoveLastPageItem.RightToLeftAutoMirrorImage = true;
+            MoveFirstPageItem.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            MovePreviousPageItem.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            MoveNextPageItem.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            MoveLastPageItem.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            CurrentPageItem.AutoSize = false;
+            CurrentPageItem.Width = 50;
+            Items.AddRange(new ToolStripItem[] { MoveFirstPageItem, MovePreviousPageItem, separator, CurrentPageItem, PageCountItem, separator2, MoveNextPageItem, MoveLastPageItem });
+        }
+
+        private void AcceptNewPage()
+        {
+            if (CurrentPageItem != null && _pageSource != null)
+            {
+                int currentPage;
+                int.TryParse(CurrentPageItem.Text, out currentPage);
+                currentPage--;
+                if (currentPage >= 0)
+                {
+                    int startIndex = currentPage * _pageSource.PageSize;
+                    if (((((startIndex != _pageSource.StartIndex) && (startIndex >= 0)) && (startIndex < _pageSource.TotalCount)) ? 1 : 0) != 0)
+                    {
+                        _pageSource.StartIndex = startIndex;
+                        if (!_pageSource.AutoRefresh) _pageSource.RefreshCurrentPage();
+                        return;
+                    }
+                }
+                CancelNewPage();
+            }
+        }
+
+        private void CancelNewPage()
+        {
+            RefreshPageProperty();
+        }
+
+        private void OnCurrentPageKey(object sender, KeyEventArgs e)
+        {
+            Keys keyCode = e.KeyCode;
+            if (keyCode != Keys.Return)
+            {
+                if (keyCode != Keys.Escape)
+                {
+                    return;
+                }
+            }
+            else
+            {
+                AcceptNewPage();
+                return;
+            }
+            CancelNewPage();
+        }
+
+        private void OnCurrentPageLostFocus(object sender, EventArgs e)
+        {
+            AcceptNewPage();
+        }
+
+        private void OnMoveFirstPage(object sender, EventArgs e)
+        {
+            if (Validate() && _pageSource != null)
+            {
+                _pageSource.StartIndex = 0;
+                if (!_pageSource.AutoRefresh) _pageSource.RefreshCurrentPage();
+            }
+        }
+
+        private void OnMoveLastPage(object sender, EventArgs e)
+        {
+            if (Validate() && _pageSource != null)
+            {
+                _pageSource.StartIndex = (_pageSource.TotalCount / _pageSource.PageSize) * _pageSource.PageSize;
+                if (!_pageSource.AutoRefresh) _pageSource.RefreshCurrentPage();
+            }
+        }
+
+        private void OnMoveNextPage(object sender, EventArgs e)
+        {
+            if (Validate() && _pageSource != null)
+            {
+                _pageSource.StartIndex += _pageSource.PageSize;
+                if (!_pageSource.AutoRefresh) _pageSource.RefreshCurrentPage();
+            }
+        }
+
+        private void OnMovePreviousPage(object sender, EventArgs e)
+        {
+            if (Validate() && _pageSource != null)
+            {
+                _pageSource.StartIndex -= _pageSource.PageSize;
+                if (!_pageSource.AutoRefresh) _pageSource.RefreshCurrentPage();
+            }
+        }
+
+
+        public void RefreshPageProperty()
+        {
+            if (_pageSource != null)
+            {
+                int pageSize = _pageSource.PageSize;
+                if (pageSize > 0)
+                {
+                    int currentPage = _pageSource.StartIndex / pageSize;
+                    int totalPage = ((_pageSource.TotalCount + pageSize) - 1) / pageSize;
+                    PageCountItem.Text = string.Format(PageCountFormat, totalPage);
+                    if (totalPage == 0)
+                    {
+                        CurrentPageItem.Text = "0";
+                    }
+                    else
+                    {
+                        CurrentPageItem.Text = Convert.ToString(currentPage + 1);
+                    }
+                    if (currentPage <= 0)
+                    {
+                        MoveFirstPageItem.Enabled = false;
+                        MovePreviousPageItem.Enabled = false;
+                    }
+                    else
+                    {
+                        MoveFirstPageItem.Enabled = true;
+                        MovePreviousPageItem.Enabled = true;
+                    }
+                    if (currentPage >= (totalPage - 1))
+                    {
+                        MoveLastPageItem.Enabled = false;
+                        MoveNextPageItem.Enabled = false;
+                    }
+                    else
+                    {
+                        MoveLastPageItem.Enabled = true;
+                        MoveNextPageItem.Enabled = true;
+                    }
+                }
+            }
+        }
+
+        private void WireUpButton(ref ToolStripItem oldButton, ToolStripItem newButton, EventHandler clickHandler)
+        {
+            if (oldButton != newButton)
+            {
+                if (oldButton != null)
+                {
+                    oldButton.Click -= clickHandler;
+                }
+                if (newButton != null)
+                {
+                    newButton.Click += clickHandler;
+                }
+                oldButton = newButton;
+            }
+        }
+
+        private void WireUpTextBox(ref ToolStripItem oldTextBox, ToolStripItem newTextBox, KeyEventHandler keyUpHandler, EventHandler lostFocusHandler)
+        {
+            if (oldTextBox != newTextBox)
+            {
+                ToolStripControlHost host = oldTextBox as ToolStripControlHost;
+                ToolStripControlHost host2 = newTextBox as ToolStripControlHost;
+                if (host != null)
+                {
+                    host.KeyUp -= keyUpHandler;
+                    host.LostFocus -= lostFocusHandler;
+                }
+                if (host2 != null)
+                {
+                    host2.KeyUp += keyUpHandler;
+                    host2.LostFocus += lostFocusHandler;
+                }
+                oldTextBox = newTextBox;
+            }
+        }
+
+        public ToolStripItem CurrentPageItem
+        {
+            get
+            {
+                return _currentPageItem;
+            }
+            set
+            {
+                WireUpTextBox(ref _currentPageItem, value, new KeyEventHandler(OnCurrentPageKey), new EventHandler(OnCurrentPageLostFocus));
+            }
+        }
+
+        public ToolStripItem MoveFirstPageItem
+        {
+            get
+            {
+                return _moveFirstPageItem;
+            }
+            set
+            {
+                WireUpButton(ref _moveFirstPageItem, value, new EventHandler(OnMoveFirstPage));
+            }
+        }
+
+        public ToolStripItem MoveLastPageItem
+        {
+            get
+            {
+                return _moveLastPageItem;
+            }
+            set
+            {
+                WireUpButton(ref _moveLastPageItem, value, new EventHandler(OnMoveLastPage));
+            }
+        }
+
+        public ToolStripItem MoveNextPageItem
+        {
+            get
+            {
+                return _moveNextPageItem;
+            }
+            set
+            {
+                WireUpButton(ref _moveNextPageItem, value, new EventHandler(OnMoveNextPage));
+            }
+        }
+
+        public ToolStripItem MovePreviousPageItem
+        {
+            get
+            {
+                return _movePreviousPageItem;
+            }
+            set
+            {
+                WireUpButton(ref _movePreviousPageItem, value, new EventHandler(OnMovePreviousPage));
+            }
+        }
+
+        public string PageCountFormat
+        {
+            get
+            {
+                return _pageCountFormat;
+            }
+            set
+            {
+                if (!(_pageCountFormat == value))
+                {
+                    _pageCountFormat = value;
+                }
+            }
+        }
+
+        public ToolStripItem PageCountItem
+        {
+            get
+            {
+                return _pageCountItem;
+            }
+            set
+            {
+                _pageCountItem = value;
+            }
+        }
+
+        public PagedBindingSource PageSource
+        {
+            get
+            {
+                return _pageSource;
+            }
+            set
+            {
+                if (value != _pageSource)
+                {
+                    if (_pageSource != null)
+                    {
+                        _pageSource.PropertyChanged -= new PropertyChangedEventHandler(PageSource_PropertyChanged);
+                    }
+                    _pageSource = value;
+                    RefreshPageProperty();
+                    if (_pageSource != null)
+                    {
+                        _pageSource.PropertyChanged += new PropertyChangedEventHandler(PageSource_PropertyChanged);
+                    }
+                }
+            }
+        }
+
+        void PageSource_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            RefreshPageProperty();
+        }
+    }
+}
+
---
+++ Samples/Sample4/Controls/PagedBindingSource.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using System.ComponentModel;
+
+namespace EE_Veloce
+{
+    public abstract class PagedBindingSource : BindingSource, INotifyPropertyChanged
+    {
+        protected const int DefaultPageSize = 50;
+        private bool _autoRefresh = true;
+        private int _totalCount;
+        private int _startIndex;
+        private int _pageSize = DefaultPageSize;
+        //private PropertyDescriptor _sortProperty;
+        //private ListSortDirection _sortDirection;
+
+        //public override bool SupportsSorting
+        //{
+        //    get { return true; }
+        //}
+
+        //public override void ApplySort(PropertyDescriptor property, ListSortDirection sort)
+        //{
+        //    if (property == _sortProperty) sort = _sortDirection ^ ListSortDirection.Descending;
+        //    _sortProperty = property;
+        //    _sortDirection = sort;
+        //    RefreshCurrentPage();
+        //}
+
+        //public override void RemoveSort()
+        //{
+        //    _sortProperty = null;
+        //    RefreshCurrentPage();
+        //}
+
+        //public override bool IsSorted
+        //{
+        //    get { return _sortProperty != null; }
+        //}
+
+        //public override PropertyDescriptor SortProperty
+        //{
+        //    get { return _sortProperty; }
+        //}
+
+        //public override ListSortDirection SortDirection
+        //{
+        //    get { return _sortDirection; }
+        //}
+
+        protected abstract object GetDataSource(int startIndex, int pageSize, PropertyDescriptor orderby, ListSortDirection direction);
+        protected abstract int GetTotalCount();
+
+        [DefaultValue(true)]
+        public bool AutoRefresh
+        {
+            get { return _autoRefresh; }
+            set { _autoRefresh = value; }
+        }
+
+        [Browsable(false), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public int TotalCount
+        {
+            get { return _totalCount; }
+        }
+
+        [DefaultValue(DefaultPageSize)]
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set
+            {
+                if (_pageSize != value)
+                {
+                    _pageSize = value;
+                    OnPropertyChanged("PageSize");
+                    if (AutoRefresh) RefreshCurrentPage();
+                }
+            }
+        }
+
+        [DefaultValue(0)]
+        public int StartIndex
+        {
+            get { return _startIndex; }
+            set
+            {
+                if (_startIndex != value)
+                {
+                    _startIndex = value;
+                    OnPropertyChanged("StartIndex");
+                    if (AutoRefresh) RefreshCurrentPage();
+                }
+            }
+        }
+
+        public override void Clear()
+        {
+            _totalCount = 0;
+            _startIndex = 0;
+            RemoveSort();            
+            base.Clear();
+            OnPropertyChanged(null);
+        }
+
+        public virtual void RefreshSource()
+        {
+            _totalCount = GetTotalCount();
+            _startIndex = 0;
+            OnPropertyChanged(null);
+            RefreshCurrentPage();
+        }
+
+        public virtual void RefreshCurrentPage()
+        {
+            DataSource = GetDataSource(StartIndex, PageSize, SortProperty, SortDirection);
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
