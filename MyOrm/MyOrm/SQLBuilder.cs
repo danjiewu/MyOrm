d2ed1ab8f3a27d2bfcde9aadad077cd1a2298d9c
danjiewu <danjiewu@hotmail.com>
2017-12-30 00:04:58 +0800

    测试

--- MyOrm/MyOrm/SQLBuilder.cs
+++ MyOrm/MyOrm/SQLBuilder.cs
@@ -4,27 +4,16 @@ using System.Text;
 using MyOrm.Common;
 using System.Text.RegularExpressions;
 using System.Collections;
+using System.Data;
+using System.Data.Common;
 
 namespace MyOrm
 {
-    public class SqlBuilder
+    /// <summary>
+    /// 生成Sql语句的辅助类
+    /// </summary>
+    public class SqlBuilder : IConditionSqlBuilder, ISqlBuilder
     {
-        #region 静态成员
-        public readonly static SqlBuilder Default = new SqlBuilder();
-        private static Dictionary<Type, SqlBuilder> sqlBuilderCache = new Dictionary<Type, SqlBuilder>();
-
-        public static SqlBuilder GetSqlBuilder(Type objectType)
-        {
-            if (sqlBuilderCache.ContainsKey(objectType)) return sqlBuilderCache[objectType];
-            return Default;
-        }
-
-        public static void RegisterSqlBuilder(Type objectType, SqlBuilder sqlBuilder)
-        {
-            sqlBuilderCache[objectType] = sqlBuilder;
-        }
-        #endregion
-
         #region 预定义变量
         /// <summary>
         /// SQL语句中like条件中的转义符
@@ -38,6 +27,8 @@ namespace MyOrm
         /// 查找列名、表名等的正则表达式
         /// </summary>
         protected static Regex sqlNameRegex = new Regex(@"\[([^\]]+)\]");
+
+        private Dictionary<Type, IConditionSqlBuilder> extCondtionBuilders = new Dictionary<Type, IConditionSqlBuilder>();
         #endregion
 
         /// <summary>
@@ -45,7 +36,17 @@ namespace MyOrm
         /// </summary>
         public TableInfoProvider Provider
         {
-            get { return DefaultConfiguration.TableInfoProvider; }
+            get { return Configuration.DefaultProvider; }
+        }
+
+        /// <summary>
+        /// 注册自定义条件SQL生成类
+        /// </summary>
+        /// <param name="conditionType">自定义条件类型</param>
+        /// <param name="conditionBuilder">自定义条件SQL生成类</param>
+        public void RegisterConditionBuilder(Type conditionType, IConditionSqlBuilder conditionBuilder)
+        {
+            extCondtionBuilders[conditionType] = conditionBuilder;
         }
 
         /// <summary>
@@ -55,7 +56,7 @@ namespace MyOrm
         /// <param name="conditon">查询条件，可为查询条件集合或单个条件，为空表示无条件</param>
         /// <param name="outputParams">供输出的参数列表，在该列表中添加SQL参数</param>
         /// <returns>生成的SQL语句，null表示无条件</returns>
-        public virtual string BuildConditionSql(SQLBuildContext context, Condition conditon, IList outputParams)
+        public virtual string BuildConditionSql(SqlBuildContext context, Condition conditon, IList outputParams)
         {
             if (conditon == null)
                 return null;
@@ -66,7 +67,13 @@ namespace MyOrm
             else if (conditon is ForeignCondition)
                 return BuildForeignConditionSql(context, conditon as ForeignCondition, outputParams);
             else
-                return BuildCustomConditionSql(context, conditon, outputParams);
+            {
+                if (extCondtionBuilders.ContainsKey(conditon.GetType()))
+                {
+                    return extCondtionBuilders[conditon.GetType()].BuildConditionSql(context, conditon, outputParams);
+                }
+                else throw new Exception(String.Format("Unsupported condition type \"{0}\"! Please register ConditionBuilder before call BuildConditionSql method.", conditon.GetType().FullName));
+            }
         }
 
         /// <summary>
@@ -76,7 +83,7 @@ namespace MyOrm
         /// <param name="condition">外部对象的查询条件</param>
         /// <param name="outputParams">供输出的参数列表，在该列表中添加SQL参数</param>
         /// <returns>生成的SQL语句，null表示无条件</returns>
-        protected string BuildForeignConditionSql(SQLBuildContext context, ForeignCondition condition, IList outputParams)
+        protected string BuildForeignConditionSql(SqlBuildContext context, ForeignCondition condition, IList outputParams)
         {
             TableDefinition tableDefinition = context.Table.Definition;
             ColumnDefinition joinedColumn = tableDefinition.GetColumn(condition.JoinedProperty);
@@ -96,20 +103,20 @@ namespace MyOrm
                 {
                     if (column.ForeignType == foreignType)
                     {
-                        if (joinedColumn != null || foreignColumn != null) throw new ArgumentException(String.Format("Uncertain relation between Type {0} and Type {1}. Please specify the ForeignCondition.JoinedProperty.", context.Table.ObjectType.FullName, foreignTable.ObjectType.FullName), "condition");
+                        if (joinedColumn != null || foreignColumn != null) throw new ArgumentException(String.Format("Uncertain relation between Type {0} and Type {1}. Please specify the ForeignCondition.JoinedProperty.", context.Table.DefinitionType.FullName, foreignTable.ObjectType.FullName), "condition");
                         joinedColumn = column;
                     }
                 }
 
                 foreach (ColumnDefinition column in foreignTable.Columns)
                 {
-                    if (column.ForeignType == context.Table.ObjectType)
+                    if (column.ForeignType == context.Table.DefinitionType)
                     {
-                        if (joinedColumn != null || foreignColumn != null) throw new ArgumentException(String.Format("Uncertain relation between Type {0} and Type {1}. Please specify the ForeignCondition.JoinedProperty.", context.Table.ObjectType.FullName, foreignTable.ObjectType.FullName), "condition");
+                        if (joinedColumn != null || foreignColumn != null) throw new ArgumentException(String.Format("Uncertain relation between Type {0} and Type {1}. Please specify the ForeignCondition.JoinedProperty.", context.Table.DefinitionType.FullName, foreignTable.ObjectType.FullName), "condition");
                         foreignColumn = column;
                     }
                 }
-                if (joinedColumn == null && foreignColumn == null) throw new ArgumentException(String.Format("No relation between Type {0} and Type {1}", context.Table.ObjectType.FullName, foreignTable.ObjectType.FullName), "condition");
+                if (joinedColumn == null && foreignColumn == null) throw new ArgumentException(String.Format("No relation between Type {0} and Type {1}", context.Table.DefinitionType.FullName, foreignTable.ObjectType.FullName), "condition");
             }
 
             if (foreignColumn == null)
@@ -119,22 +126,22 @@ namespace MyOrm
             }
             else if (joinedColumn == null)
             {
-                if (context.Table.Definition.Keys.Count != 1) throw new ArgumentException(String.Format("Type \"{0}\" does not support foreign condition,which only take effect on type with one and only key column.", context.Table.ObjectType.FullName), "condition");
+                if (context.Table.Definition.Keys.Count != 1) throw new ArgumentException(String.Format("Type \"{0}\" does not support foreign condition,which only take effect on type with one and only key column.", context.Table.DefinitionType.FullName), "condition");
                 joinedColumn = context.Table.Definition.Keys[0];
             }
 
             string tableAlias = context.TableAliasName ?? context.Table.Name;
-            string foreignTableAlias = "T" + context.JoinIndex;
+            string foreignTableAlias = "T" + context.Sequence;
 
-            return String.Format("{0} exists (select 1 from [{1}] [{2}] where [{3}].[{4}] = [{5}].[{6}] and ({7}))",
-                condition.Opposite ? "not" : null,
+            return String.Format("{0}exists (select 1 from [{1}] [{2}] where [{3}].[{4}] = [{5}].[{6}] and ({7}))",
+                condition.Opposite ? "not " : null,
                 foreignTable.Name,
                 foreignTableAlias,
                 tableAlias,
                 joinedColumn.PropertyName,
                 foreignTableAlias,
                 foreignColumn.Name,
-                SqlBuilder.GetSqlBuilder(foreignType).BuildConditionSql(new SQLBuildContext() { TableAliasName = foreignTableAlias, JoinIndex = context.JoinIndex + 1, Table = foreignTable }, condition.Condition, outputParams));
+                BuildConditionSql(new SqlBuildContext() { TableAliasName = foreignTableAlias, Sequence = context.Sequence + 1, Table = foreignTable }, condition.Condition, outputParams));
         }
 
         /// <summary>
@@ -144,7 +151,7 @@ namespace MyOrm
         /// <param name="conditionSet">查询条件的集合</param>
         /// <param name="outputParams">供输出的参数列表，在该列表中添加SQL参数</param>
         /// <returns>生成的SQL语句，null表示无条件</returns>
-        protected string BuildConditionSetSql(SQLBuildContext context, ConditionSet conditionSet, IList outputParams)
+        protected string BuildConditionSetSql(SqlBuildContext context, ConditionSet conditionSet, IList outputParams)
         {
             List<string> conditions = new List<string>();
             foreach (Condition subConditon in conditionSet.SubConditions)
@@ -163,14 +170,14 @@ namespace MyOrm
         /// <param name="simpleCondition">简单查询条件</param>
         /// <param name="outputParams">参数列表，在该列表中添加SQL参数</param>
         /// <returns>生成的SQL语句</returns>
-        protected string BuildSimpleConditionSql(SQLBuildContext context, SimpleCondition simpleCondition, IList outputParams)
+        protected string BuildSimpleConditionSql(SqlBuildContext context, SimpleCondition simpleCondition, IList outputParams)
         {
             Column column = context.Table.GetColumn(simpleCondition.Property);
-            if (column == null) throw new Exception(String.Format("Property \"{0}\" does not exist in type \"{1}\".", simpleCondition.Property, context.Table.ObjectType.FullName));
+            if (column == null) throw new Exception(String.Format("Property \"{0}\" does not exist in type \"{1}\".", simpleCondition.Property, context.Table.DefinitionType.FullName));
 
             string tableAlias = context.TableAliasName;
-            string columnName = tableAlias == null ? column.FormattedExpression : String.Format("[{0}].[{1}]", tableAlias, column.Name);
-            string expression = simpleCondition.ExpressionFormat == null ? columnName : String.Format(simpleCondition.ExpressionFormat, columnName);
+            string columnName = tableAlias == null ? column.FormattedExpression(this) : String.Format("[{0}].[{1}]", tableAlias, column.Name);
+            string expression = columnName;
             object value = simpleCondition.Value;
 
             string strOpposite = simpleCondition.Opposite ? "not" : null;
@@ -183,25 +190,13 @@ namespace MyOrm
                 value = sqlLike.Replace(Convert.ToString(value), LikeEscapeChar + "$1");
             switch (simpleCondition.Operator)
             {
-                case ConditionOperator.Constant: return expression;
                 case ConditionOperator.Equals: return String.Format(simpleCondition.Opposite ? "{0} <> {1}" : "{0} = {1}", expression, ToSqlParam(outputParams.Add(value).ToString()));
                 case ConditionOperator.LargerThan: return String.Format(simpleCondition.Opposite ? "{0} <= {1}" : "{0} > {1}", expression, ToSqlParam(outputParams.Add(value).ToString()));
                 case ConditionOperator.SmallerThan: return String.Format(simpleCondition.Opposite ? "{0} >= {1}" : "{0} < {1}", expression, ToSqlParam(outputParams.Add(value).ToString()));
                 case ConditionOperator.Like: return String.Format(@"{0} {1} like {2}", expression, strOpposite, ToSqlParam(outputParams.Add(value).ToString()));
-#if MYSQL
-                case ConditionOperator.StartsWith: return String.Format(@"{0} {1} like CONCAT({2}, '%') escape '{3}'", expression, strOpposite, ToSqlParam(outputParams.Add(value).ToString()), LikeEscapeChar);
-                case ConditionOperator.EndsWith: return String.Format(@"{0} {1} like CONCAT('%', {2}) escape '{3}'", expression, strOpposite, ToSqlParam(outputParams.Add(value).ToString()), LikeEscapeChar);
-                case ConditionOperator.Contains: return String.Format(@"{0} {1} like CONCAT('%', {2}, '%') escape '{3}'", expression, strOpposite, ToSqlParam(outputParams.Add(value).ToString()), LikeEscapeChar);
-#elif ORACLE
-                case ConditionOperator.StartsWith: return String.Format(@"{0} {1} like {2} || '%' escape '{3}'", expression, strOpposite, ToSqlParam(outputParams.Add(value).ToString()), LikeEscapeChar);
-                case ConditionOperator.EndsWith: return String.Format(@"{0} {1} like '%' || {2} escape '{3}'", expression, strOpposite, ToSqlParam(outputParams.Add(value).ToString()), LikeEscapeChar);
-                case ConditionOperator.Contains: return String.Format(@"{0} {1} like '%' || {2} || '%' escape '{3}'", expression, strOpposite, ToSqlParam(outputParams.Add(value).ToString()), LikeEscapeChar);
-
-#else
-                case ConditionOperator.StartsWith: return String.Format(@"{0} {1} like {2} + '%' escape '{3}'", expression, strOpposite, ToSqlParam(outputParams.Add(value).ToString()), LikeEscapeChar);
-                case ConditionOperator.EndsWith: return String.Format(@"{0} {1} like '%' + {2} escape '{3}'", expression, strOpposite, ToSqlParam(outputParams.Add(value).ToString()), LikeEscapeChar);
-                case ConditionOperator.Contains: return String.Format(@"{0} {1} like '%' + {2} + '%' escape '{3}'", expression, strOpposite, ToSqlParam(outputParams.Add(value).ToString()), LikeEscapeChar);
-#endif
+                case ConditionOperator.StartsWith: return String.Format(@"{0} {1} like {2} escape '{3}'", expression, strOpposite, ConcatSql(ToSqlParam(outputParams.Add(value).ToString()), "'%'"), LikeEscapeChar);
+                case ConditionOperator.EndsWith: return String.Format(@"{0} {1} like {2} escape '{3}'", expression, strOpposite, ConcatSql("'%'", ToSqlParam(outputParams.Add(value).ToString())), LikeEscapeChar);
+                case ConditionOperator.Contains: return String.Format(@"{0} {1} like {2} escape '{3}'", expression, strOpposite, ConcatSql("'%'", ToSqlParam(outputParams.Add(value).ToString()), "'%'"), LikeEscapeChar);
                 case ConditionOperator.In:
                     List<string> paramNames = new List<string>();
                     foreach (object item in value as IEnumerable)
@@ -215,15 +210,42 @@ namespace MyOrm
         }
 
         /// <summary>
-        /// 根据自定义条件生成SQL语句与SQL参数
+        /// 生成带标识列的插入SQL
         /// </summary>
-        /// <param name="context">用来生成SQL的上下文</param>
-        /// <param name="customConditon">自定义的查询条件</param>
-        /// <param name="outputParams">供输出的参数列表，在该列表中添加SQL参数</param>
-        /// <returns>生成的SQL语句，null表示无条件</returns>
-        protected virtual string BuildCustomConditionSql(SQLBuildContext context, Condition customConditon, IList outputParams)
+        /// <param name="command">SQLCommand</param>
+        /// <param name="identityColumn">标识列</param>
+        /// <param name="tableName">表名</param>
+        /// <param name="strColumns">插入数据列名称</param>
+        /// <param name="strValues">插入数据名称</param>
+        /// <returns></returns>
+        public virtual string BuildIdentityInsertSQL(IDbCommand command,ColumnDefinition identityColumn, string tableName, string strColumns, string strValues)
         {
-            throw new Exception(String.Format("Unknown condition type \"{0}\"! Please override the \"BuildCustomConditionSql\" method.", customConditon.GetType().FullName));
+            return String.Format("BEGIN insert into {0} ({1}) values ({2}); {3} END;", tableName, strColumns, strValues, "select @@IDENTITY as [ID];");
+        }
+
+        /// <summary>
+        /// 连接各字符串的SQL语句
+        /// </summary>
+        /// <param name="strs">需要连接的sql字符串</param>
+        /// <returns>SQL语句</returns>
+        protected virtual string ConcatSql(params string[] strs)
+        {
+            return String.Join(" + ", strs);
+        }
+
+        /// <summary>
+        /// 生成分页查询的SQL语句
+        /// </summary>
+        /// <param name="select">select内容</param>
+        /// <param name="from">from块</param>
+        /// <param name="where">where条件</param>
+        /// <param name="orderBy">排序</param>
+        /// <param name="startIndex">起始位置，从0开始</param>
+        /// <param name="sectionSize">查询条数</param>
+        /// <returns></returns>
+        public virtual string GetSelectSectionSql(string select, string from, string where, string orderBy, int startIndex, int sectionSize)
+        {
+            return String.Format("select * from (select {0}, Row_Number() over (Order by {1}) as Row_Number from {2} where {3}) TempTable where Row_Number > {4} and Row_Number <= {5}", select, orderBy, from, where, startIndex, startIndex + sectionSize);
         }
 
         /// <summary>
@@ -233,7 +255,8 @@ namespace MyOrm
         /// <returns>数据库合法名称</returns>
         public virtual string ToSqlName(string name)
         {
-            return String.Format("[{0}]", name);
+            if (name == null) throw new ArgumentNullException("name");
+            return String.Join(".", Array.ConvertAll(name.Split('.'), n => String.Format("[{0}]", n)));
         }
 
         /// <summary>
@@ -253,11 +276,7 @@ namespace MyOrm
         /// <returns>参数名称</returns>
         public virtual string ToParamName(string nativeName)
         {
-#if MYSQL
-            return String.Format("@{0}", nativeName);
-#else
             return nativeName;
-#endif
         }
 
         /// <summary>
@@ -267,30 +286,27 @@ namespace MyOrm
         /// <returns>原始名称</returns>
         public virtual string ToNativeName(string paramName)
         {
-#if MYSQL
-            return paramName.TrimStart('@');
-#else
             return paramName;
-#endif
         }
 
         /// <summary>
         /// 将列名、表名等替换为数据库合法名称
         /// </summary>
-        /// <param name="sql"></param>
+        /// <param name="sql">sql语句</param>
         /// <returns></returns>
-        public string ReplaceSqlName(string sql)
+        public virtual string ReplaceSqlName(string sql)
         {
-#if MYSQL
-            return ReplaceSqlName(sql, '`', '`');
-#elif ORACLE
-            return ReplaceSqlName(sql, '"', '"');
-#else
             return sql;
-#endif
         }
 
-        private static string ReplaceSqlName(string sql, char left, char right)
+        /// <summary>
+        /// 将列名、表名等替换为数据库合法名称
+        /// </summary>
+        /// <param name="sql">sql语句</param>
+        /// <param name="left">左定界符</param>
+        /// <param name="right">右定界符</param>
+        /// <returns></returns>
+        protected string ReplaceSqlName(string sql, char left, char right)
         {
             if (sql == null) return null;
             StringBuilder sb = new StringBuilder();
@@ -326,10 +342,18 @@ namespace MyOrm
         }
     }
 
-    public class SQLBuildContext
+    /// <summary>
+    /// 自定义Condition转换为sql语句的接口
+    /// </summary>
+    public interface IConditionSqlBuilder
     {
-        public string TableAliasName { get; set; }
-        public Table Table { get; set; }
-        public int JoinIndex { get; set; }
+        /// <summary>
+        /// 生成sql语句
+        /// </summary>
+        /// <param name="context">生成sql的上下文</param>
+        /// <param name="customConditon">自定义Condition</param>
+        /// <param name="outputParams">存放参数的集合</param>
+        /// <returns>生成的sql字符串</returns>
+        string BuildConditionSql(SqlBuildContext context, Condition customConditon, IList outputParams);
     }
 }
